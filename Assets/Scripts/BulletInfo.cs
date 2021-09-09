4f4923e6f70d2bc1df753397114c052724ac2734
coded-21 <coded.ha21@gmail.com>
2021-09-09 09:18:05 +0530

    added bullet vfx + bullet collision code

--- Assets/Scripts/BulletInfo.cs
+++ Assets/Scripts/BulletInfo.cs
@@ -9,13 +9,27 @@ public class BulletInfo : MonoBehaviour
     [HideInInspector] public int damage;
     [HideInInspector] public float bulletSpeed;
     [HideInInspector] public Vector3 direction;
+    public GameObject hitVFX;
+    Rigidbody rb;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void FixedUpdate()
     {
-        GetComponent<Transform>().Translate(direction * Time.deltaTime * bulletSpeed);
+        rb.velocity = bulletSpeed * Time.deltaTime * direction;
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Player with view ID " + shooter.ViewID + " hit: " + other.gameObject.name + " with a damage of " + damage);
+        ContactPoint _contactPoint = collision.contacts[0];
+        ProcessHit(_contactPoint);
+        Debug.Log("Player with view ID " + shooter.ViewID + " hit: " + collision.gameObject.name + " with a damage of " + damage);
+    }
+
+    private void ProcessHit(ContactPoint contactPoint)
+    {
+        PhotonNetwork.Instantiate(hitVFX.name, contactPoint.point, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
